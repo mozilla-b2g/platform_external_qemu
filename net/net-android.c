vicamo/b2g_platform_external_qemu

Bug 991452 - Add wlan0 interface for wifi test. r=vicamo

net-android.c
@@ -2268,6 +2268,24 @@ int net_client_init(Monitor *mon, const char *device, const char *p)
         snprintf(buf, sizeof(buf), "rmnet.%u", rmnet_index++);
         nd->name = qemu_strdup(buf);
     } else
+    if (!strcmp(device, "wlan")) {
+        static unsigned int wlan_index = 0;
+
+        if (name) {
+            config_error(mon, "wlan iface should not have customized name.\n");
+            ret = -1;
+            goto out;
+        }
+
+        ret = net_client_init(mon, "nic", p);
+        if (ret < 0) {
+            return ret;
+        }
+
+        NICInfo *nd = &nd_table[ret];
+        snprintf(buf, sizeof(buf), "wlan.%u", wlan_index++);
+        nd->name = qemu_strdup(buf);
+    } else
     if (!strcmp(device, "none")) {
         if (*p != '\0') {
             config_error(mon, "'none' takes no parameters\n");

mozilla-b2g/platform_external_qemu

net/net-android.c
@@ -2244,6 +2244,24 @@ int net_client_init(Monitor *mon, const char *device, const char *p)
         snprintf(buf, sizeof(buf), "rmnet.%u", rmnet_index++);
         nd->name = g_strdup(buf);
     } else
+    if (!strcmp(device, "wlan")) {
+        static unsigned int wlan_index = 0;
+
+        if (name) {
+            config_error(mon, "wlan iface should not have customized name.\n");
+            ret = -1;
+            goto out;
+        }
+
+        ret = net_client_init(mon, "nic", p);
+        if (ret < 0) {
+            return ret;
+        }
+
+        NICInfo *nd = &nd_table[ret];
+        snprintf(buf, sizeof(buf), "wlan.%u", wlan_index++);
+        nd->name = g_strdup(buf);
+    } else
     if (!strcmp(device, "none")) {
         if (*p != '\0') {
             config_error(mon, "'none' takes no parameters\n");